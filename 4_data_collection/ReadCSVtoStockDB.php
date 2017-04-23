2ca3ff451f6815b62f86213f55fa7ceb6a0e4b9d
amr331 <tonyrodriguez2004@gmail.com>
2017-04-23 12:39:08 -0400

    Add files via upload
    New file which creates historical prices and also the Tickers table which lists a company name and the corresponding tickerid

---
+++ 4_data_collection/ReadCSVtoStockDB.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Read Historic Stock Price Data</h1>
+
+<?php
+//This module will read from a CSV file and save the data into the Stock Table.
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "StockDBPDO";
+$path = "/wamp64/www/Hist";
+$id = array("AAPL", "AIG", "AMD", "AZO", "BP", "CI", "DB", "EA", "F", "FB", "FDX");
+$stockname = array("Apple Computer, Inc.","American International Group Inc", "Advanced Micro Devices, Inc.",
+	"AutoZone, Inc.", "BP plc (ADR)", "CIGNA Corporation", "Deutsche Bank AG (USA)", "Electronic Arts Inc.",
+	"Ford Motor Company", "Facebook Inc", "FedEx Corporation");
+
+$row = 1;
+for ($i = 0; $i < count($id); $i++){
+if (($handle = fopen("$path/$id[$i].csv", "r")) !== FALSE) {
+    echo "<p> Reading data from $id[$i].csv<br /></p>\n";
+    echo "<p> Updating Table StockPrices...<br /></p>\n";
+    while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
+        $num = count($data);
+        $row++;
+        $s = $data[0];
+        $dt = new DateTime($s);
+        $dat = $dt->format('Y-m-d');
+    
+    try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO StockPrices (tickerid, stockname, date, closingprice, volume)
+    VALUES ('$id[$i]', '$stockname[$i]', '$dat', $data[1], $data[5])";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+
+$conn = null;
+
+    }
+    echo "<p> Update Complete<br /></p>\n";
+    fclose($handle);
+}
+}
+echo "<p> Stock Prices Update Completed<br /></p>\n";
+
+//This module will read from a CSV file and save the data into the Tickers Table.
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "StockDBPDO";
+$path = "/wamp64/www/Hist";
+$id = "Tickers1";
+
+$row = 1;
+
+if (($handle = fopen("$path/$id.csv", "r")) !== FALSE) {
+    echo "<p> Reading data from $id.csv<br /></p>\n";
+    echo "<p> Updating Table Tickers1...<br /></p>\n";
+    while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
+        $num = count($data);
+        $row++;
+            
+    try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO Tickers (tickerid, stockname)
+    VALUES ('$data[0]', '$data[1]')";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+
+$conn = null;
+
+    }
+    echo "<p> Update Complete<br /></p>\n";
+    fclose($handle);
+
+}
+echo "<p> Stock Prices Update Completed<br /></p>\n";
+
+?>
+
+</body>
+</html>
